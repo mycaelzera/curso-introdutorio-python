470ebe12e7091e287915ae9d431390f4502005f7
Mycael Moreira <mycaelsantos26@gmail.com>
2025-06-02 19:11:16 -0300

    refactor: listas.py

--- dia_04/listas.py
+++ dia_04/listas.py
@@ -3,6 +3,34 @@
 idades = [28, 42, 43, 35, 39, 28, 38]
 print(idades)
 
-# teste de alteração direto do github
-# alterado no vscode
-nome = "Mycael"
+# %%
+mycael = ["Mycael", "Moreira", 20, False, "Solteiro", 1500.0]
+print(mycael)
+
+# %%
+type(mycael)
+
+# %%
+
+# idade
+mycael[2]
+
+# renda
+print(mycael[5])
+
+print(mycael[0])
+
+# %%
+
+idades = [28, 42, 43, 35, 39, 28, 38, 42, 34]
+
+# funções
+print("Soma idades: ", sum(idades))
+
+print("Qtde idades:", len(idades))
+
+print("Média idades: ", sum(idades) / len(idades))
+
+print("Menor idade: ", min(idades))
+
+print("Maior idade: ", max(idades))
